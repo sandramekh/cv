13e31faccb17e99a44d3977dbbd397d0067c075c
Lesya <kuksia@i.ua>
2016-04-07 13:34:51 +0000

    initial commiy

--- script/js.js
+++ script/js.js
@@ -1,1 +1,11 @@
-// JavaScript File
+/* global $*/
+$(function (){
+    //jQuery for page scrolling - requires jQuery Easing plugin
+    $('a.page-scroll').bind ('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop ().animate ({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easyInOutExpo');
+        event.preventDefault();
+    });
+})
