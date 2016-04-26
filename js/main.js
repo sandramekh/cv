e113e0ca7193d23f80dcc22ae76c748a2019d549
Lesya <kuksia@i.ua>
2016-04-26 08:02:08 +0000

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-//JavaScript File
 /*global $*/
 $(function () {
     // jQuery for page scrolling feature - requires jQuery Easing plugin
@@ -9,19 +8,4 @@ $(function () {
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
-    
-    var wow = new WOW(
-  {
-    boxClass:     'wow',      // animated element css class (default is wow)
-    animateClass: 'animated wow zoomInUp', // animation css class (default is animated)
-    offset:       0,          // distance to the element when triggering the animation (default is 0)
-    mobile:       true,       // trigger animations on mobile devices (default is true)
-    live:         true,       // act on asynchronously loaded content (default is true)
-    callback:     function(box) {
-      // the callback is fired every time an animation is started
-      // the argument that is passed in is the DOM node being animated
-    },
-    scrollContainer: null // optional scroll container selector, otherwise use window
-  }
-);
-wow.init();
+})
