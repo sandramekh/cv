a8c9e3dde1716915d0d5e4eb2a3f38c1adc72fd4
Lesya <kuksia@i.ua>
2016-05-10 11:04:09 +0000

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
-/*global $*/
+
+$(document).ready(function(){
+  $("#responsive-menu, #arrow").on("click","a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+    //забираем идентификатор бока с атрибута href
+    var id  = $(this).attr('href'),
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({scrollTop: top}, 1500);
+  })
+  /*global $*/
 $(function () {
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
